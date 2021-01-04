8b801a7856e12e550334b058b044ce05d91b8b2c
AnastasK2 <ZaporozhetsAnastasia12@gmail.com>
2021-01-05 00:16:10 +0300

    Обновила сайт с портфолио

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+    pagination: {
+        el: '.projects-pagination',
+        bulletClass: 'projects-bullet',
+        bulletActiveClass: 'projects-bullet-active',
+        clickable: true
+    },
+});
+
+$(document).ready(function(){
+    $("#yak").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+            top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1000);
+    });
+});
